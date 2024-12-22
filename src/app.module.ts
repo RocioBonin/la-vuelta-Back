0137b56d0c73d992e9814b50979aa2b1a4225899
Rocio <rociobonindev@gmail.com>
2024-12-22 15:37:16 -0300

    deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { postgresDataSourceConfig } from './config/data-source';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: './.env.development'
+      envFilePath: './.env.development',
+      load: [postgresDataSourceConfig],
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        database: configService.get('DB_NAME'),
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        /* entities: [], */
-        synchronize: true,
-      })
+      useFactory: (configService: ConfigService) => 
+        configService.get('postgres'),
     })
   ],
   controllers: [],
